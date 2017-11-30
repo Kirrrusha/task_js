257da796e54bcd20a18ffa6bdd2fc4c2183b8c6a
kirilllebedenko@gmail.com <kirilllebedenko@gmail.com>
2017-11-30 17:45:55 +0300

    1

--- 20.js
+++ 20.js
@@ -16,24 +16,3 @@ function reliableMultiply(a, b) {
 }
 
 console.log(reliableMultiply(8, 8));
-// → 64
-
-//console.log(primitiveMultiply(2, 2));
-
-/* function AssertionFailed(message) {
-    this.message = message;
-}
-AssertionFailed.prototype = Object.create(Error.prototype);
-
-function assert(test, message) {
-    if (!test)
-        throw new AssertionFailed(message);
-}
-
-function lastElement(array) {
-    assert(array.length > 0, "пустой массив в lastElement");
-    return array[array.length - 1];
-}
-
-
-console.log(lastElement([])); */
